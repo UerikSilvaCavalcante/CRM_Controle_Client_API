429b754cd34dfbb558c94f97fcf83d550523c286
Uerik <uerik.silva@academico.ifg.edu.br>
2025-12-01 00:52:16 -0300

    Fix: docker-compose

--- setup/settings.py
+++ setup/settings.py
@@ -168,7 +168,7 @@ DEFAULT_FROM_EMAIL = str(os.getenv("EMAIL"))
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
